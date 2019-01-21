3a332313c16aa4fa5f8125c1eea53dcd8a53d267
yona <alenastr12@gmail.com>
2019-01-21 17:15:58 +0200

    #028 implemented ThymeleafViewResolver

--- src/main/java/com/stroganova/onlineshop/web/controller/ProductController.java
+++ src/main/java/com/stroganova/onlineshop/web/controller/ProductController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,19 +22,16 @@ public class ProductController {
     private final Logger LOGGER = LoggerFactory.getLogger(getClass());
 
     @RequestMapping(value = "/products", method = RequestMethod.GET)
-    public String getAll(@ModelAttribute("model") ModelMap model, @RequestAttribute("session") Session session) {
+    public String getAll(Model model) {
         model.addAttribute("products", productService.getAll());
-        model.addAttribute("session", session);
         LOGGER.info("Show all products");
         return "products";
-
     }
 
 
     @RequestMapping(path = "/products/add", method = RequestMethod.GET)
-    public String openAddProductForm(@ModelAttribute("model") ModelMap model, @RequestAttribute("session") Session session) {
+    public String openAddProductForm() {
         LOGGER.info("Open Add Product Form");
-        model.addAttribute("session", session);
         return "add";
     }
 
@@ -47,18 +45,15 @@ public class ProductController {
 
 
     @RequestMapping(path = "/cart", method = RequestMethod.GET)
-    public String openCart(@ModelAttribute("model") ModelMap model, @RequestAttribute("session") Session session) throws IOException {
-        if (session != null) {
-            model.addAttribute("cart", session.getCart());
-        }
+    public String openCart(Model model, @RequestAttribute("session") Session session) throws IOException {
+        model.addAttribute("cart", session.getCart());
         LOGGER.info("Show the cart");
         return "cart";
     }
 
 
     @RequestMapping(path = "/cart", method = RequestMethod.POST)
-    public String addToCart(@RequestAttribute("session") Session session, @RequestParam(name = "id") long id) {
-        Product product = productService.getProduct(id);
+    public String addToCart(@RequestAttribute("session") Session session, @ModelAttribute Product product) {
         session.addToCart(product);
         return "redirect:/products";
     }
